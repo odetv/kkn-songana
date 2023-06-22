a8d168e35740884a6f5bcfc514d9fe966bff8f49
odetv <abdiutama02@gmail.com>
2023-06-22 17:29:10 +0800

    updated contents

--- src/pages/Tentang.tsx
+++ src/pages/Tentang.tsx
@@ -9,200 +9,200 @@ const people = [
         role: "Koordinator Desa",
         imageUrl:
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1606164886-1673048476.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Komang Ayu Laras Swati",
         role: "Wakil Koordinator Desa",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/7977361281685075648.SAVE_20220601_191508.jpg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Ni Putu Ashya Siva Marshanda",
         role: "Sekretaris 1",
         imageUrl:
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/494792644-1676541672.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Kadek Yesi Citrayani",
         role: "Sekretaris 2",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17024534901686742346._MG_0822%20(Master).jpg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Putu Ayodhia Darsinita",
         role: "Bendahara 1",
         imageUrl:
             "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/0801c61d8fb7d10db0e1418870878e9c20210617030646.jpg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Made Putrayasa",
         role: "Bendahara 2",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21305203471685343532.Md%20Putrayasa%20xngt%20(1).JPG",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Ida Ayu Putu Krisnayani Putri",
         role: "Sie Acara & Humas",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/1627714691685350600.IMG_20201227_153855-removebg-preview.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Ni Kadek Tia Pratiwi",
         role: "Sie Acara & Humas",
         imageUrl:
             "http://static1.undiksha.ac.id/siakng/2022/fotomahasiswa/1485454036-1643432426.jpeg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "St. Nur Septiani Chandra",
         role: "Sie Acara & Humas",
         imageUrl:
             "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/1399186221-1630394685.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Dewa Gede Andika Andara Putra",
         role: "Sie Perlengkapan",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21200058061685349105.DSC_8239-removebg-preview.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Kadek Rio Teguh Adnyana",
         role: "Sie Perlengkapan",
         imageUrl:
             "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/b9b081d42c77de27f03d017d19ac142b20210504060541.jpg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Putu Surya Ardi Bhuana",
         role: "Sie Perlengkapan",
         imageUrl:
             "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/158406574-1630624756.jpeg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "I Gede Gelgel Abdiutama",
         role: "Sie Perlengkapan",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/14298581461685158842.FOTO%20JAS%20UNDIKSHA.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Made Adith Mahendra",
         role: "Sie Perlengkapan",
         imageUrl:
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1227541198-1677907503.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Kadek Ariana",
         role: "Sie Pubdekdok",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21300049351685321862.foto%20kkn%20undiksha%202023.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "I Komang Ngurah Yasana",
         role: "Sie Pubdekdok",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/15442142121687058418.IMG_20230526_230938-removebg-preview.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Dewa Putu Dharma Sudana",
         role: "Sie Pubdekdok",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/16991335661685254487.IMG_3160.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Zivi S Sitanggang",
         role: "Sie Konsumsi",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/9049465431685344337.IMG_7491%20copy.jpg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Indra Dwisaputra",
         role: "Sie Konsumsi",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3406693481685151635.Foto_Profil_2-removebg-preview.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Varelly Arletta Tyora Putri Herlambang",
         role: "Sie Konsumsi",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/19463217111686819503.Ber_jas_latar_merah.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Ketut Gita Ariani",
         role: "Sie Konsumsi",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/20652141041685273200.WhatsApp%20Image%202023-05-28%20at%2019.20.19.jpeg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Luluk Mukarromah",
         role: "Sie Konsumsi",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/2547369561684966226.0FFF47AA-1A7E-4CB0-A505-E5D86176282C.jpeg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Ida Polanda Nenepat",
         role: "Sie Kerohanian",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17814276801683025478.Picsart_23-05-02_19-03-13-454.png",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "Ni Kadek Marini Arianti",
         role: "Sie Kerohanian",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/856534681685273179.IMG_8991%20(1).jpg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
     {
         name: "I Gede Teguh Brahmastra Wiradharma",
         role: "Sie Kerohanian",
         imageUrl:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3913884821685331243.1.jpg",
-        linkPhoto: "",
-        linkCard: "",
+        linkPhoto: "/photo",
+        linkCard: "/idcard",
     },
 ];
 
