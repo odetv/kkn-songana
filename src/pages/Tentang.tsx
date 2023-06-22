b264f832d82bed4dde433e2a4a07dbbed36e3e1a
odetv <abdiutama02@gmail.com>
2023-06-22 19:08:45 +0800

    updated contents

--- src/pages/Tentang.tsx
+++ src/pages/Tentang.tsx
@@ -3,32 +3,6 @@ import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { UserCircleIcon, IdentificationIcon } from "@heroicons/react/20/solid";
 
-import AndraFahreza from "../assets/images/tentang/id-card/AndraFahreza.png";
-import KomangAyuLarasSwati from "../assets/images/tentang/id-card/KomangAyuLarasSwati.png";
-import NiPutuAshyaSivaMarshanda from "../assets/images/tentang/id-card/NiPutuAshyaSivaMarshanda.png";
-import KadekYesiCitrayani from "../assets/images/tentang/id-card/KadekYesiCitrayani.png";
-import PutuAyodhiaDarsinita from "../assets/images/tentang/id-card/PutuAyodhiaDarsinita.png";
-import MadePutrayasa from "../assets/images/tentang/id-card/MadePutrayasa.png";
-import IdaAyuPutuKrisnayaniPutri from "../assets/images/tentang/id-card/IdaAyuPutuKrisnayaniPutri.png";
-import NiKadekTiaPratiwi from "../assets/images/tentang/id-card/NiKadekTiaPratiwi.png";
-import StNurSeptianiChandra from "../assets/images/tentang/id-card/StNurSeptianiChandra.png";
-import DewaGedeAndikaAndaraPutra from "../assets/images/tentang/id-card/DewaGedeAndikaAndaraPutra.png";
-import KadekRioTeguhAdnyana from "../assets/images/tentang/id-card/KadekRioTeguhAdnyana.png";
-import PutuSuryaArdiBhuana from "../assets/images/tentang/id-card/PutuSuryaArdiBhuana.png";
-import IGedeGelgelAbdiutama from "../assets/images/tentang/id-card/IGedeGelgelAbdiutama.png";
-import MadeAdithMahendra from "../assets/images/tentang/id-card/MadeAdithMahendra.png";
-import KadekAriana from "../assets/images/tentang/id-card/KadekAriana.png";
-import IKomangNgurahYasana from "../assets/images/tentang/id-card/IKomangNgurahYasana.png";
-import DewaPutuDharmaSudana from "../assets/images/tentang/id-card/DewaPutuDharmaSudana.png";
-import ZiviSSitanggang from "../assets/images/tentang/id-card/ZiviSSitanggang.png";
-import IndraDwisaputra from "../assets/images/tentang/id-card/IndraDwisaputra.png";
-import VarellyArlettaTyoraPutriHerlambang from "../assets/images/tentang/id-card/VarellyArlettaTyoraPutriHerlambang.png";
-import KetutGitaAriani from "../assets/images/tentang/id-card/KetutGitaAriani.png";
-import LulukMukarromah from "../assets/images/tentang/id-card/LulukMukarromah.png";
-import IdaPolandaNenepat from "../assets/images/tentang/id-card/IdaPolandaNenepat.png";
-import NiKadekMariniArianti from "../assets/images/tentang/id-card/NiKadekMariniArianti.png";
-import IGedeTeguhBrahmastraWiradharma from "../assets/images/tentang/id-card/IGedeTeguhBrahmastraWiradharma.png";
-
 const people = [
     {
         name: "Andra Fahreza",
@@ -37,7 +11,7 @@ const people = [
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1606164886-1673048476.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1606164886-1673048476.png",
-        linkCard: "src/assets/images/tentang/id-card/AndraFahreza.png",
+        linkCard: "/src/assets/images/tentang/id-card/AndraFahreza.png",
     },
     {
         name: "Komang Ayu Laras Swati",
@@ -46,7 +20,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/7977361281685075648.SAVE_20220601_191508.jpg",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/7977361281685075648.SAVE_20220601_191508.jpg",
-        linkCard: { KomangAyuLarasSwati },
+        linkCard: "/src/assets/images/tentang/id-card/KomangAyuLarasSwati.png",
     },
     {
         name: "Ni Putu Ashya Siva Marshanda",
@@ -55,7 +29,8 @@ const people = [
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/494792644-1676541672.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/494792644-1676541672.png",
-        linkCard: { NiPutuAshyaSivaMarshanda },
+        linkCard:
+            "/src/assets/images/tentang/id-card/NiPutuAshyaSivaMarshanda.png",
     },
     {
         name: "Kadek Yesi Citrayani",
@@ -64,7 +39,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17024534901686742346._MG_0822%20(Master).jpg",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17024534901686742346._MG_0822%20(Master).jpg",
-        linkCard: { KadekYesiCitrayani },
+        linkCard: "/src/assets/images/tentang/id-card/KadekYesiCitrayani.png",
     },
     {
         name: "Putu Ayodhia Darsinita",
@@ -73,7 +48,7 @@ const people = [
             "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/0801c61d8fb7d10db0e1418870878e9c20210617030646.jpg",
         linkPhoto:
             "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/0801c61d8fb7d10db0e1418870878e9c20210617030646.jpg",
-        linkCard: { PutuAyodhiaDarsinita },
+        linkCard: "/src/assets/images/tentang/id-card/PutuAyodhiaDarsinita.png",
     },
     {
         name: "Made Putrayasa",
@@ -82,7 +57,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21305203471685343532.Md%20Putrayasa%20xngt%20(1).JPG",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21305203471685343532.Md%20Putrayasa%20xngt%20(1).JPG",
-        linkCard: { MadePutrayasa },
+        linkCard: "/src/assets/images/tentang/id-card/MadePutrayasa.png",
     },
     {
         name: "Ida Ayu Putu Krisnayani Putri",
@@ -91,7 +66,8 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/1627714691685350600.IMG_20201227_153855-removebg-preview.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/1627714691685350600.IMG_20201227_153855-removebg-preview.png",
-        linkCard: { IdaAyuPutuKrisnayaniPutri },
+        linkCard:
+            "/src/assets/images/tentang/id-card/IdaAyuPutuKrisnayaniPutri.png",
     },
     {
         name: "Ni Kadek Tia Pratiwi",
@@ -100,7 +76,7 @@ const people = [
             "http://static1.undiksha.ac.id/siakng/2022/fotomahasiswa/1485454036-1643432426.jpeg",
         linkPhoto:
             "http://static1.undiksha.ac.id/siakng/2022/fotomahasiswa/1485454036-1643432426.jpeg",
-        linkCard: { NiKadekTiaPratiwi },
+        linkCard: "/src/assets/images/tentang/id-card/NiKadekTiaPratiwi.png",
     },
     {
         name: "St. Nur Septiani Chandra",
@@ -109,7 +85,7 @@ const people = [
             "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/1399186221-1630394685.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/1399186221-1630394685.png",
-        linkCard: { StNurSeptianiChandra },
+        linkCard: "/src/assets/images/tentang/id-card/StNurSeptianiChandra.png",
     },
     {
         name: "Dewa Gede Andika Andara Putra",
@@ -118,7 +94,8 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21200058061685349105.DSC_8239-removebg-preview.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21200058061685349105.DSC_8239-removebg-preview.png",
-        linkCard: { DewaGedeAndikaAndaraPutra },
+        linkCard:
+            "/src/assets/images/tentang/id-card/DewaGedeAndikaAndaraPutra.png",
     },
     {
         name: "Kadek Rio Teguh Adnyana",
@@ -127,7 +104,7 @@ const people = [
             "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/b9b081d42c77de27f03d017d19ac142b20210504060541.jpg",
         linkPhoto:
             "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/b9b081d42c77de27f03d017d19ac142b20210504060541.jpg",
-        linkCard: { KadekRioTeguhAdnyana },
+        linkCard: "/src/assets/images/tentang/id-card/KadekRioTeguhAdnyana.png",
     },
     {
         name: "Putu Surya Ardi Bhuana",
@@ -136,7 +113,7 @@ const people = [
             "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/158406574-1630624756.jpeg",
         linkPhoto:
             "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/158406574-1630624756.jpeg",
-        linkCard: { PutuSuryaArdiBhuana },
+        linkCard: "/src/assets/images/tentang/id-card/PutuSuryaArdiBhuana.png",
     },
     {
         name: "I Gede Gelgel Abdiutama",
@@ -145,7 +122,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/14298581461685158842.FOTO%20JAS%20UNDIKSHA.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/14298581461685158842.FOTO%20JAS%20UNDIKSHA.png",
-        linkCard: { IGedeGelgelAbdiutama },
+        linkCard: "/src/assets/images/tentang/id-card/IGedeGelgelAbdiutama.png",
     },
     {
         name: "Made Adith Mahendra",
@@ -154,7 +131,7 @@ const people = [
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1227541198-1677907503.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1227541198-1677907503.png",
-        linkCard: { MadeAdithMahendra },
+        linkCard: "/src/assets/images/tentang/id-card/MadeAdithMahendra.png",
     },
     {
         name: "Kadek Ariana",
@@ -163,7 +140,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21300049351685321862.foto%20kkn%20undiksha%202023.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21300049351685321862.foto%20kkn%20undiksha%202023.png",
-        linkCard: { KadekAriana },
+        linkCard: "/src/assets/images/tentang/id-card/KadekAriana.png",
     },
     {
         name: "I Komang Ngurah Yasana",
@@ -172,7 +149,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/15442142121687058418.IMG_20230526_230938-removebg-preview.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/15442142121687058418.IMG_20230526_230938-removebg-preview.png",
-        linkCard: { IKomangNgurahYasana },
+        linkCard: "/src/assets/images/tentang/id-card/IKomangNgurahYasana.png",
     },
     {
         name: "Dewa Putu Dharma Sudana",
@@ -181,7 +158,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/16991335661685254487.IMG_3160.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/16991335661685254487.IMG_3160.png",
-        linkCard: { DewaPutuDharmaSudana },
+        linkCard: "/src/assets/images/tentang/id-card/DewaPutuDharmaSudana.png",
     },
     {
         name: "Zivi S Sitanggang",
@@ -190,7 +167,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/9049465431685344337.IMG_7491%20copy.jpg",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/9049465431685344337.IMG_7491%20copy.jpg",
-        linkCard: { ZiviSSitanggang },
+        linkCard: "/src/assets/images/tentang/id-card/ZiviSSitanggang.png",
     },
     {
         name: "Indra Dwisaputra",
@@ -199,7 +176,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3406693481685151635.Foto_Profil_2-removebg-preview.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3406693481685151635.Foto_Profil_2-removebg-preview.png",
-        linkCard: { IndraDwisaputra },
+        linkCard: "/src/assets/images/tentang/id-card/IndraDwisaputra.png",
     },
     {
         name: "Varelly Arletta Tyora Putri Herlambang",
@@ -208,7 +185,8 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/19463217111686819503.Ber_jas_latar_merah.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/19463217111686819503.Ber_jas_latar_merah.png",
-        linkCard: { VarellyArlettaTyoraPutriHerlambang },
+        linkCard:
+            "/src/assets/images/tentang/id-card/VarellyArlettaTyoraPutriHerlambang.png",
     },
     {
         name: "Ketut Gita Ariani",
@@ -217,7 +195,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/20652141041685273200.WhatsApp%20Image%202023-05-28%20at%2019.20.19.jpeg",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/20652141041685273200.WhatsApp%20Image%202023-05-28%20at%2019.20.19.jpeg",
-        linkCard: { KetutGitaAriani },
+        linkCard: "/src/assets/images/tentang/id-card/KetutGitaAriani.png",
     },
     {
         name: "Luluk Mukarromah",
@@ -226,7 +204,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/2547369561684966226.0FFF47AA-1A7E-4CB0-A505-E5D86176282C.jpeg",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/2547369561684966226.0FFF47AA-1A7E-4CB0-A505-E5D86176282C.jpeg",
-        linkCard: { LulukMukarromah },
+        linkCard: "/src/assets/images/tentang/id-card/LulukMukarromah.png",
     },
     {
         name: "Ida Polanda Nenepat",
@@ -235,7 +213,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17814276801683025478.Picsart_23-05-02_19-03-13-454.png",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17814276801683025478.Picsart_23-05-02_19-03-13-454.png",
-        linkCard: { IdaPolandaNenepat },
+        linkCard: "/src/assets/images/tentang/id-card/IdaPolandaNenepat.png",
     },
     {
         name: "Ni Kadek Marini Arianti",
@@ -244,7 +222,7 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/856534681685273179.IMG_8991%20(1).jpg",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/856534681685273179.IMG_8991%20(1).jpg",
-        linkCard: { NiKadekMariniArianti },
+        linkCard: "/src/assets/images/tentang/id-card/NiKadekMariniArianti.png",
     },
     {
         name: "I Gede Teguh Brahmastra Wiradharma",
@@ -253,7 +231,8 @@ const people = [
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3913884821685331243.1.jpg",
         linkPhoto:
             "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3913884821685331243.1.jpg",
-        linkCard: { IGedeTeguhBrahmastraWiradharma },
+        linkCard:
+            "/src/assets/images/tentang/id-card/IGedeTeguhBrahmastraWiradharma.png",
     },
 ];
 
