b84aaf5b2e45ab50e3478ecdf99eeb1700dd504f
odetv <abdiutama02@gmail.com>
2023-06-22 20:43:45 +0800

    updated contents

--- src/pages/Tentang.tsx
+++ src/pages/Tentang.tsx
@@ -8,27 +8,27 @@ const people = [
         name: "Andra Fahreza",
         role: "Koordinator Desa",
         imageUrl:
-            "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1606164886-1673048476.png",
+            "https://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1606164886-1673048476.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1606164886-1673048476.png",
+            "https://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1606164886-1673048476.png",
         linkCard: "/src/assets/images/tentang/id-card/AndraFahreza.png",
     },
     {
         name: "Komang Ayu Laras Swati",
         role: "Wakil Koordinator Desa",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/7977361281685075648.SAVE_20220601_191508.jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/7977361281685075648.SAVE_20220601_191508.jpg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/7977361281685075648.SAVE_20220601_191508.jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/7977361281685075648.SAVE_20220601_191508.jpg",
         linkCard: "/src/assets/images/tentang/id-card/KomangAyuLarasSwati.png",
     },
     {
         name: "Ni Putu Ashya Siva Marshanda",
         role: "Sekretaris 1",
         imageUrl:
-            "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/494792644-1676541672.png",
+            "https://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/494792644-1676541672.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/494792644-1676541672.png",
+            "https://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/494792644-1676541672.png",
         linkCard:
             "/src/src/assets/images/tentang/id-card/NiPutuAshyaSivaMarshanda.png",
     },
@@ -36,36 +36,36 @@ const people = [
         name: "Kadek Yesi Citrayani",
         role: "Sekretaris 2",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17024534901686742346._MG_0822%20(Master).jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/17024534901686742346._MG_0822%20(Master).jpg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17024534901686742346._MG_0822%20(Master).jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/17024534901686742346._MG_0822%20(Master).jpg",
         linkCard: "/src/assets/images/tentang/id-card/KadekYesiCitrayani.png",
     },
     {
         name: "Putu Ayodhia Darsinita",
         role: "Bendahara 1",
         imageUrl:
-            "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/0801c61d8fb7d10db0e1418870878e9c20210617030646.jpg",
+            "https://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/0801c61d8fb7d10db0e1418870878e9c20210617030646.jpg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/0801c61d8fb7d10db0e1418870878e9c20210617030646.jpg",
+            "https://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/0801c61d8fb7d10db0e1418870878e9c20210617030646.jpg",
         linkCard: "/src/assets/images/tentang/id-card/PutuAyodhiaDarsinita.png",
     },
     {
         name: "Made Putrayasa",
         role: "Bendahara 2",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21305203471685343532.Md%20Putrayasa%20xngt%20(1).JPG",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/21305203471685343532.Md%20Putrayasa%20xngt%20(1).JPG",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21305203471685343532.Md%20Putrayasa%20xngt%20(1).JPG",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/21305203471685343532.Md%20Putrayasa%20xngt%20(1).JPG",
         linkCard: "/src/assets/images/tentang/id-card/MadePutrayasa.png",
     },
     {
         name: "Ida Ayu Putu Krisnayani Putri",
         role: "Sie Acara & Humas",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/1627714691685350600.IMG_20201227_153855-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/1627714691685350600.IMG_20201227_153855-removebg-preview.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/1627714691685350600.IMG_20201227_153855-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/1627714691685350600.IMG_20201227_153855-removebg-preview.png",
         linkCard:
             "/src/assets/images/tentang/id-card/IdaAyuPutuKrisnayaniPutri.png",
     },
@@ -73,27 +73,27 @@ const people = [
         name: "Ni Kadek Tia Pratiwi",
         role: "Sie Acara & Humas",
         imageUrl:
-            "http://static1.undiksha.ac.id/siakng/2022/fotomahasiswa/1485454036-1643432426.jpeg",
+            "https://static1.undiksha.ac.id/siakng/2022/fotomahasiswa/1485454036-1643432426.jpeg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/siakng/2022/fotomahasiswa/1485454036-1643432426.jpeg",
+            "https://static1.undiksha.ac.id/siakng/2022/fotomahasiswa/1485454036-1643432426.jpeg",
         linkCard: "/src/assets/images/tentang/id-card/NiKadekTiaPratiwi.png",
     },
     {
         name: "St. Nur Septiani Chandra",
         role: "Sie Acara & Humas",
         imageUrl:
-            "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/1399186221-1630394685.png",
+            "https://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/1399186221-1630394685.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/1399186221-1630394685.png",
+            "https://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/1399186221-1630394685.png",
         linkCard: "/src/assets/images/tentang/id-card/StNurSeptianiChandra.png",
     },
     {
         name: "Dewa Gede Andika Andara Putra",
         role: "Sie Perlengkapan",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21200058061685349105.DSC_8239-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/21200058061685349105.DSC_8239-removebg-preview.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21200058061685349105.DSC_8239-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/21200058061685349105.DSC_8239-removebg-preview.png",
         linkCard:
             "/src/assets/images/tentang/id-card/DewaGedeAndikaAndaraPutra.png",
     },
@@ -101,90 +101,90 @@ const people = [
         name: "Kadek Rio Teguh Adnyana",
         role: "Sie Perlengkapan",
         imageUrl:
-            "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/b9b081d42c77de27f03d017d19ac142b20210504060541.jpg",
+            "https://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/b9b081d42c77de27f03d017d19ac142b20210504060541.jpg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/b9b081d42c77de27f03d017d19ac142b20210504060541.jpg",
+            "https://static1.undiksha.ac.id/daftarkembali/2022/photoUploads/b9b081d42c77de27f03d017d19ac142b20210504060541.jpg",
         linkCard: "/src/assets/images/tentang/id-card/KadekRioTeguhAdnyana.png",
     },
     {
         name: "Putu Surya Ardi Bhuana",
         role: "Sie Perlengkapan",
         imageUrl:
-            "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/158406574-1630624756.jpeg",
+            "https://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/158406574-1630624756.jpeg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/158406574-1630624756.jpeg",
+            "https://static1.undiksha.ac.id/siakng/2021/fotomahasiswa/158406574-1630624756.jpeg",
         linkCard: "/src/assets/images/tentang/id-card/PutuSuryaArdiBhuana.png",
     },
     {
         name: "I Gede Gelgel Abdiutama",
         role: "Sie Perlengkapan",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/14298581461685158842.FOTO%20JAS%20UNDIKSHA.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/14298581461685158842.FOTO%20JAS%20UNDIKSHA.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/14298581461685158842.FOTO%20JAS%20UNDIKSHA.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/14298581461685158842.FOTO%20JAS%20UNDIKSHA.png",
         linkCard: "/src/assets/images/tentang/id-card/IGedeGelgelAbdiutama.png",
     },
     {
         name: "Made Adith Mahendra",
         role: "Sie Perlengkapan",
         imageUrl:
-            "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1227541198-1677907503.png",
+            "https://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1227541198-1677907503.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1227541198-1677907503.png",
+            "https://static1.undiksha.ac.id/siakng/2023/fotomahasiswa/1227541198-1677907503.png",
         linkCard: "/src/assets/images/tentang/id-card/MadeAdithMahendra.png",
     },
     {
         name: "Kadek Ariana",
         role: "Sie Pubdekdok",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21300049351685321862.foto%20kkn%20undiksha%202023.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/21300049351685321862.foto%20kkn%20undiksha%202023.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/21300049351685321862.foto%20kkn%20undiksha%202023.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/21300049351685321862.foto%20kkn%20undiksha%202023.png",
         linkCard: "/src/assets/images/tentang/id-card/KadekAriana.png",
     },
     {
         name: "I Komang Ngurah Yasana",
         role: "Sie Pubdekdok",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/15442142121687058418.IMG_20230526_230938-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/15442142121687058418.IMG_20230526_230938-removebg-preview.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/15442142121687058418.IMG_20230526_230938-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/15442142121687058418.IMG_20230526_230938-removebg-preview.png",
         linkCard: "/src/assets/images/tentang/id-card/IKomangNgurahYasana.png",
     },
     {
         name: "Dewa Putu Dharma Sudana",
         role: "Sie Pubdekdok",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/16991335661685254487.IMG_3160.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/16991335661685254487.IMG_3160.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/16991335661685254487.IMG_3160.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/16991335661685254487.IMG_3160.png",
         linkCard: "/src/assets/images/tentang/id-card/DewaPutuDharmaSudana.png",
     },
     {
         name: "Zivi S Sitanggang",
         role: "Sie Konsumsi",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/9049465431685344337.IMG_7491%20copy.jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/9049465431685344337.IMG_7491%20copy.jpg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/9049465431685344337.IMG_7491%20copy.jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/9049465431685344337.IMG_7491%20copy.jpg",
         linkCard: "/src/assets/images/tentang/id-card/ZiviSSitanggang.png",
     },
     {
         name: "Indra Dwisaputra",
         role: "Sie Konsumsi",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3406693481685151635.Foto_Profil_2-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/3406693481685151635.Foto_Profil_2-removebg-preview.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3406693481685151635.Foto_Profil_2-removebg-preview.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/3406693481685151635.Foto_Profil_2-removebg-preview.png",
         linkCard: "/src/assets/images/tentang/id-card/IndraDwisaputra.png",
     },
     {
         name: "Varelly Arletta Tyora Putri Herlambang",
         role: "Sie Konsumsi",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/19463217111686819503.Ber_jas_latar_merah.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/19463217111686819503.Ber_jas_latar_merah.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/19463217111686819503.Ber_jas_latar_merah.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/19463217111686819503.Ber_jas_latar_merah.png",
         linkCard:
             "/src/assets/images/tentang/id-card/VarellyArlettaTyoraPutriHerlambang.png",
     },
@@ -192,45 +192,45 @@ const people = [
         name: "Ketut Gita Ariani",
         role: "Sie Konsumsi",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/20652141041685273200.WhatsApp%20Image%202023-05-28%20at%2019.20.19.jpeg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/20652141041685273200.WhatsApp%20Image%202023-05-28%20at%2019.20.19.jpeg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/20652141041685273200.WhatsApp%20Image%202023-05-28%20at%2019.20.19.jpeg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/20652141041685273200.WhatsApp%20Image%202023-05-28%20at%2019.20.19.jpeg",
         linkCard: "/src/assets/images/tentang/id-card/KetutGitaAriani.png",
     },
     {
         name: "Luluk Mukarromah",
         role: "Sie Konsumsi",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/2547369561684966226.0FFF47AA-1A7E-4CB0-A505-E5D86176282C.jpeg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/2547369561684966226.0FFF47AA-1A7E-4CB0-A505-E5D86176282C.jpeg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/2547369561684966226.0FFF47AA-1A7E-4CB0-A505-E5D86176282C.jpeg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/2547369561684966226.0FFF47AA-1A7E-4CB0-A505-E5D86176282C.jpeg",
         linkCard: "/src/assets/images/tentang/id-card/LulukMukarromah.png",
     },
     {
         name: "Ida Polanda Nenepat",
         role: "Sie Kerohanian",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17814276801683025478.Picsart_23-05-02_19-03-13-454.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/17814276801683025478.Picsart_23-05-02_19-03-13-454.png",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/17814276801683025478.Picsart_23-05-02_19-03-13-454.png",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/17814276801683025478.Picsart_23-05-02_19-03-13-454.png",
         linkCard: "/src/assets/images/tentang/id-card/IdaPolandaNenepat.png",
     },
     {
         name: "Ni Kadek Marini Arianti",
         role: "Sie Kerohanian",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/856534681685273179.IMG_8991%20(1).jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/856534681685273179.IMG_8991%20(1).jpg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/856534681685273179.IMG_8991%20(1).jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/856534681685273179.IMG_8991%20(1).jpg",
         linkCard: "/src/assets/images/tentang/id-card/NiKadekMariniArianti.png",
     },
     {
         name: "I Gede Teguh Brahmastra Wiradharma",
         role: "Sie Kerohanian",
         imageUrl:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3913884821685331243.1.jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/3913884821685331243.1.jpg",
         linkPhoto:
-            "http://static1.undiksha.ac.id/kkn/2023/upload/profile/3913884821685331243.1.jpg",
+            "https://static1.undiksha.ac.id/kkn/2023/upload/profile/3913884821685331243.1.jpg",
         linkCard:
             "/src/assets/images/tentang/id-card/IGedeTeguhBrahmastraWiradharma.png",
     },
@@ -269,7 +269,7 @@ export default function Tentang() {
                         </h1>
                         <img
                             className="h-16 w-16 rounded-full object-cover mb-2"
-                            src="http://dosen.undiksha.ac.id/admin/media/ujm/dosen/0012088606/0012088606_photo_i_made_edy_listartha.jpg"
+                            src="https://dosen.undiksha.ac.id/admin/media/ujm/dosen/0012088606/0012088606_photo_i_made_edy_listartha.jpg"
                             onContextMenu={(e) => e.preventDefault()}
                             draggable="false"
                         />
