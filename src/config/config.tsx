c04e5c0f595f59fd7e5ec7146c100b4f21b1bcab
odetv <abdiutama02@gmail.com>
2023-07-17 16:18:42 +0800

    updated config

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,2 +1,2 @@
 // export const url = "http://localhost:8000"; //Dev
-export const url = "https://blog-api.momentkitabali.my.id"; //Prod
+export const url = "https://api-kknsongana.momentkitabali.my.id"; //Prod
